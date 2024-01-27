8860ab25b7e0f2c04ad2fa020d4b54f369d9ef6e
ayhonz <janmachacek97@gmail.com>
2024-01-27 13:57:54 +0100

    editor refactor

--- editor/editor.go
+++ editor/editor.go
@@ -13,17 +13,37 @@ const (
 	MODE_EDIT
 )
 
+const (
+	KEY_H = 104
+	KEY_J = 106
+	KEY_K = 107
+	KEY_L = 108
+)
+
 type Editor struct {
 	screen tcell.Screen
 	buffer string // just a joke!
 	mode   Mode
 	file   string
+	style  *Style
+	cursor cursor
 }
 
-// probably should split this one into init and run
-func (ed *Editor) Run() {
-	defStyle := tcell.StyleDefault.Background(tcell.ColorReset).Foreground(tcell.ColorReset)
-	boxStyle := tcell.StyleDefault.Foreground(tcell.ColorWhite).Background(tcell.ColorReset)
+type Style struct {
+	defaultStyle tcell.Style
+	boxStyle     tcell.Style
+}
+
+type cursor struct {
+	x int
+	y int
+}
+
+func Init() *Editor {
+	style := &Style{
+		defaultStyle: tcell.StyleDefault.Background(tcell.ColorReset).Foreground(tcell.ColorReset),
+		boxStyle:     tcell.StyleDefault.Foreground(tcell.ColorWhite).Background(tcell.ColorReset),
+	}
 
 	s, err := tcell.NewScreen()
 	if err != nil {
@@ -33,39 +53,53 @@ func (ed *Editor) Run() {
 		log.Fatalf("%+v", err)
 	}
 
-	s.SetStyle(defStyle)
+	s.SetStyle(style.defaultStyle)
 	s.Clear()
 
-	ed.screen = s
+	cursor := cursor{
+		x: 1,
+		y: 1,
 
-	s.ShowCursor(1, 1)
-	x, y := 1, 1
+	}
 
+	return &Editor{
+		screen: s,
+		buffer: "Hello there my friend",
+		mode:   MODE_NORMAL,
+		style:  style,
+		cursor: cursor,
+	}
+
+}
+
+// probably should split this one into init and run
+func (ed *Editor) Run() {
 	for {
-		s.Show()
-		ed.DrawText(1, 1, 42, 7, boxStyle, "Hello")
-		event := s.PollEvent()
+		ed.screen.Show()
+		ed.DrawText(1, 1, 42, 7, ed.style.boxStyle, ed.buffer)
+		ed.screen.ShowCursor(ed.cursor.x, ed.cursor.y)
+		event := ed.screen.PollEvent()
 
 		switch eventType := event.(type) {
 		case *tcell.EventKey:
 			if eventType.Key() == tcell.KeyCtrlC {
 				return
 			}
-			if eventType.Rune() == 108  { // l
-				x += 1
-				s.ShowCursor(x, y)
+			if eventType.Rune() == KEY_L { // l
+				ed.cursor.x += 1
+				ed.screen.ShowCursor(ed.cursor.x, ed.cursor.y)
 			}
-			if eventType.Rune() == 104  { // h 
-				x -= 1
-				s.ShowCursor(x, y)
+			if eventType.Rune() == KEY_H { // h
+				ed.cursor.x -= 1
+				ed.screen.ShowCursor(ed.cursor.x, ed.cursor.y)
 			}
-			if eventType.Rune() == 106  { // j 
-				y += 1
-				s.ShowCursor(x, y)
+			if eventType.Rune() == KEY_J { // j
+				ed.cursor.y += 1
+				ed.screen.ShowCursor(ed.cursor.x, ed.cursor.y)
 			}
-			if eventType.Rune() == 107  { // k
-				y -= 1
-				s.ShowCursor(x, y)
+			if eventType.Rune() == KEY_K { // k
+				ed.cursor.y -= 1
+				ed.screen.ShowCursor(ed.cursor.x, ed.cursor.y)
 			}
 			// mod, key, ch, name := eventType.Modifiers(), eventType.Key(), eventType.Rune(), eventType.Name()
 			// log.Fatalf("EventKey Modifiers: %d Key: %d Rune: %d, Name: %s", mod, key, ch, name)
--- main.go
+++ main.go
@@ -3,8 +3,7 @@ package main
 import "github.com/ayhonz/termix/editor"
 
 func main() {
-	var editor editor.Editor // do I have to do this?
-
-	defer editor.Quit()
-	editor.Run()
+	e := editor.Init()	
+	defer e.Quit()
+	e.Run()
 }
